9daad2fc60bf9d596000e61874a3a114f3402d1b
Viacheslav Egorenkov <ve@poloniumarts.com>
2013-11-01 09:57:17 +0400

    [rem]Garbage code

--- NetUtilsSamples/src/com/example/netutilssamples/SimpleMapping.java
+++ NetUtilsSamples/src/com/example/netutilssamples/SimpleMapping.java
@@ -1,12 +1,12 @@
 package com.example.netutilssamples;
 
-import java.lang.reflect.Field;
 import java.util.GregorianCalendar;
 
 import ru.poloniumarts.netutils.ViewMapper;
 import android.app.Activity;
-import android.util.Log;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.RadioButton;
 
 import com.example.netutilssamples.entities.Person;
 import com.googlecode.androidannotations.annotations.AfterViews;
@@ -31,9 +31,4 @@ public class SimpleMapping extends Activity {
 		person.set("John", "Doe", "Example", new GregorianCalendar(1979, GregorianCalendar.JANUARY, 21).getTime(), "http://neogrotesque.net/wp-content/uploads/avatar-6.jpg");
 		mapper.map(root, person);
 	}
-}
-
-class TransientFields{
-	public int				someInt;
-	public transient int	someTransient;
 }
